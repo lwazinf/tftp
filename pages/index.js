ab5d808476f7a279b7dec20119a861194f42bb93
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2021-01-01 06:16:03 +0200

    New UI

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
-import Image from "next/image";
 import ReactRoundedImage from "react-rounded-image";
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
 
 
 
 export default function Home() {
-  
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
   return (
    <>
-   
+
+      <div className={styles.slider}>
+
+        <AutoplaySlider play={true} cancelOnInteraction={false} interval={8000} buttons={false} bullets={false} showTimer={false}>
+
+          <div data-src="/Spin.jpg" />
+          <div data-src="/Paint.jpg" />
+          <div data-src="/Quads.png" />
+          <div data-src="/Rides.jpg" />
+
+        </AutoplaySlider>
+
+      </div>
+      <div className={styles.brands}></div>
+      <div className={styles.logo}>
+        <Image src={"/logo.png"} width={250} height={250} alt={"logo"} />
+      </div>
+
     {/* FEATURES */}
 
     <div className={styles.features}> 
