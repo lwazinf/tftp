c1734fde1f89be446626d8fbb91a3119dd88904c
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2022-07-22 18:02:20 +0200

    NB :: Upload & Share

--- Desktop/playground/rounded_/components/DC01_.tsx
+++ Desktop/playground/rounded_/components/DC01_.tsx
@@ -1,3 +1,6 @@
+import { faShare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
 import { TwitterShareButton } from "react-twitter-embed";
 import { useRecoilState } from "recoil";
 import { commentState } from "./atoms/atoms";
@@ -7,7 +10,34 @@ import ControlsObj_ from "./elements/01_/ControlsObj_";
 interface DC01_Props {}
 
 const DC01_ = ({}: DC01_Props) => {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const [comments_, setComments_] = useRecoilState(commentState);
+  const start = new Date();
+  const start_ = {
+    month: month[start.getMonth()],
+    day: start.getDay(),
+    year: start.getFullYear(),
+  };
+  const inputFile = useRef(null);
+
+  const onButtonClick = () => {
+    // `current` points to the mounted file input element
+   inputFile.current.click();
+  };
+
   return (
     <div
       className={`h-[50px] w-[800px] flex flex-col items-center justify-center relative`}
@@ -16,19 +46,28 @@ const DC01_ = ({}: DC01_Props) => {
         className={`w-[550px] h-[200px] rounded-lg shade bg-white flex flex-row relative justify-start items-center p-1`}
       >
         <div
-          className={`w-full h-full absolute top-0 right-0 flex flex-col justify-center items-center overflow-hidden`}
+          className={`w-full h-full absolute top-0 right-0 flex flex-col justify-center items-center overflow-hidden pointer-events-none`}
         >
           <img
             src={`/assets/images/LwaziNF.png`}
-            className={`w-[150px] relative left-[150px] top-[50px] opacity-10`}
+            className={`w-[150px] relative left-[150px] top-[50px] opacity-10 pointer-events-none`}
           />
         </div>
+        <input
+          type="file"
+          id="file"
+          ref={inputFile}
+          style={{ display: "none" }}
+        />
         <div
-          className={`w-[250px] h-full bg-gray-500 rounded-lg overflow-hidden cursor-pointer`}
+          className={`w-[250px] h-full rounded-lg overflow-hidden cursor-pointer`}
+          onClick={() => {
+            inputFile.current.click();
+          }}
         >
           <img
             src={`https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}
-            className={`h-full object-cover`}
+            className={`h-full object-cover opacity-100 hover:opacity-80 transition-all duration-500`}
           />
         </div>
         <div
@@ -37,23 +76,27 @@ const DC01_ = ({}: DC01_Props) => {
           <div
             className={`w-[300px] h-[120px] flex flex-col justify-center items-center`}
           >
-            <div className={`w-[285px] h-[50px] flex flex-row`}>
-              <p className={`text-[14px] text-black/80 font-medium`}>
-                What style of shoe are you into? A favorite colour maybe?!
-              </p>
+            <div
+              className={`w-[285px] h-[45px] flex flex-row relative overflow-hidden`}
+            >
+              <textarea
+                className={`text-[14px] text-black/80 w-[95%] font-medium`}
+                rows={2}
+                spellCheck={false}
+                placeholder={`What style of shoe are you into? A favorite colour maybe?!`}
+                onChange={(e) => {}}
+              ></textarea>
             </div>
             <div
-              className={`w-[280px] h-[70px] flex flex-row relative overflow-hidden`}
+              className={`w-[280px] h-[75px] flex flex-row relative overflow-hidden`}
             >
-              <p
-                className={`text-[11px] text-black/50 font-light text-ellipsis`}
-              >
-                Non cupidatat tempor reprehenderit cillum consectetur ullamco et
-                cupidatat sunt sit ullamco. Aute nostrud ex exercitation do et
-                qui cupidatat qui consectetur proident pariatur. Commodo laborum
-                veniam dolor qui laboris ex. Officia dolor elit mollit aliquip
-                quis enim irure et id qui nostrud aliquip.
-              </p>
+              <textarea
+                rows={5}
+                spellCheck={false}
+                className={`text-[12px] text-black/50 font-light w-[280px] min-h-[70px] bg-transparent relative top-0`}
+                placeholder={`Non cupidatat tempor reprehenderit cillum consectetur ullamco et cupidatat sunt sit ullamco. Aute nostrud ex exercitation do et qui cupidatat qui consectetur proident pariatur.`}
+                onChange={(e) => {}}
+              ></textarea>
             </div>
           </div>
           <div
@@ -76,32 +119,39 @@ const DC01_ = ({}: DC01_Props) => {
                 Lwazi Ndlovu
               </p>
               <p className={`text-[10px] text-black/50 font-normal`}>
-                Jun 20, 2022
+                {start_["month"]} {start_["day"]}, {start_["year"]}
               </p>
             </div>
             <div
               className={`w-[20px] relative right-[50px] top-[5px] opacity-70`}
             >
-              <TwitterShareButton
-                onLoad={() => {}}
-                options={{
-                  buttonHashtag: undefined,
-                  screenName: undefined,
-                  size: "small",
-                  text: "Testing",
-                  via: "LwaziNF",
-                }}
-                placeholder={
-                  <div>
-                    {/* <FontAwesomeIcon
+              <FontAwesomeIcon
+                icon={faShare}
+                className={`h-[14px] w-[14px] text-gray-500/50 relative right-[-50px] bottom-[0px] transition-all duration-500 hover:text-gray-500 cursor-pointer`}
+                onClick={() => {}}
+              />
+              <div className={`hidden absolute top-0`} id="twitterButton">
+                <TwitterShareButton
+                  onLoad={() => {}}
+                  options={{
+                    buttonHashtag: undefined,
+                    screenName: undefined,
+                    size: "small",
+                    text: "Testing",
+                    via: "LwaziNF",
+                  }}
+                  placeholder={
+                    <div>
+                      {/* <FontAwesomeIcon
                     icon={faShare}
                     className={`h-[14px] w-[14px] text-gray-500/50 transition-all duration-500 hover:text-gray-500 my-1 relative right-[-1.5px] bottom-[-4px] cursor-pointer`}
                     onClick={() => {}}
                   /> */}
-                  </div>
-                }
-                url="https://lwazinf.com"
-              />
+                    </div>
+                  }
+                  url="https://lwazinf.com"
+                />
+              </div>
             </div>
           </div>
         </div>
